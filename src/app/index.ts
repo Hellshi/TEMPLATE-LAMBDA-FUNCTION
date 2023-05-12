d517fbcfd00efbb0d8fd0da89fcefb8a119ead9b
Hellshi <hcslimaa@gmail.com>
2023-05-12 09:30:52 -0300

    feat: trying to implement layers

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,3 +1,5 @@
+//@ts-ignore
+import moment from 'moment'
 // TODO: move to layers should be the last thing you will do
 export const handler = async (event) => {
     return {
@@ -5,7 +7,7 @@ export const handler = async (event) => {
       body: JSON.stringify(
         {
           message: 'Go Serverless v3.0! Your function executed successfully!',
-          input: "moment",
+          input: moment().format(),
         },
         null,
         2
