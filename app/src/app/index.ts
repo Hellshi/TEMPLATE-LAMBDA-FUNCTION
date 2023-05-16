ed7a9a41698bbb466b00f40076e001aef27cb397
Hellshi <hcslimaa@gmail.com>
2023-05-16 09:35:45 -0300

    fix: layers

--- app/src/app/index.ts
+++ app/src/app/index.ts
@@ -1,5 +1,5 @@
 //@ts-ignore
-import moment from 'moment'
+import * as moment from 'moment';
 // TODO: move to layers should be the last thing you will do
 export const handler = async (event) => {
     return {
@@ -7,7 +7,7 @@ export const handler = async (event) => {
       body: JSON.stringify(
         {
           message: 'Go Serverless v3.0! Your function executed successfully!',
-          input: moment().format(),
+          input: moment('2016-01-01T23:35:01').format(),
         },
         null,
         2
